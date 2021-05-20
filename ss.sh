574a002ae01728c93008cd95f86c452c71dcc21e
“joshbav” <josh@laptop.home>
2021-05-19 17:01:26 -0700

    Scripted commit 19-May-2021-05:01:26PM

--- ss.sh
+++ ss.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i ~/joshb.pem -o ConnectTimeout=30 -o ConnectionAttempts=4 -o StrictHostKeyChecking=no centos@$1
+ssh -i ~/aws-personal.pem -o ConnectTimeout=30 -o ConnectionAttempts=4 -o StrictHostKeyChecking=no %1
