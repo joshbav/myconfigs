af78982a35ed0f8f3a2e54cbd08aea4a81abe610
“joshbav” <joshbav@users.noreply.github.com>
2021-06-12 16:10:12 -0700

    Scripted commit 12-Jun-2021-04:10:12PM

--- build.sh
+++ build.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
-# Backups up some of my ~/. files
+# Backups up some of my ~/. files to enable easy swapping of workstations
 
 # TODO: Fork what they did: https://github.com/SuperPaintman/dotfiles
 
-
 echo
-echo Creating encrypted backup of VS Code settings, enter password
-mv vscode.7z /tmp 2>&1
-7z a -t7z -p vscode ~/.vscode/settings.json -r
+echo
+echo Creating encrypted backup of much of ~
+echo Use the long dedicated 7zip password
+echo
+mv secretstuff.7z /tmp 2>&1
+
+# I'm sure I'll forget to add new stuff here, D'OH! in advance
+7z a -t7z -p -r secretstuff ~/.vscode ~/.kube ~/.docker ~./gnupg ~./vmware ~./config ~./ssr ~./zoom ~./ssh ~./gitconfig 
+# to extract use 7z e  (it will preserve paths)
 
 cp ~/.bash_profile .
 cp ~/.vimrc .
 cp ~/.bash_aliases .
 cp ~/.bashrc .
-
-#echo Now doing SSH config, but I stopped using this file a while ago
-#echo enter password if asked
-#7z a -t7z -p sshconfig ~/.ssh/config
-
-# TODO iterm config file
-# zoom config file?
-# git config encrypted though?
-
-# don't need kubeconfig backed up using this method, because I won't curl it
-# to a new workstation, right?  same with cloud provider CLIs?
-
 
 echo
 echo
