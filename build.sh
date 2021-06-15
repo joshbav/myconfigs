ca2552e33c442392b8319a0accf48f20e32809fa
“joshbav” <joshbav@users.noreply.github.com>
2021-06-14 20:45:45 -0700

    Scripted commit 14-Jun-2021-08:45:45PM

--- build.sh
+++ build.sh
@@ -12,7 +12,8 @@ echo
 mv secretstuff.7z /tmp 2>&1
 
 # I'm sure I'll forget to add new stuff here, D'OH! in advance
-7z a -t7z -p -r secretstuff ~/.vscode ~/.kube ~/.docker ~./gnupg ~./vmware ~./config ~./ssr ~./zoom ~./ssh ~./gitconfig 
+7z a -t7z -p -r secretstuff ~/aws-personal.pem ~/.vscode ~/.kube ~/.docker ~./gnupg ~./vmware ~./pki ~./config ~./ssr ~./zoom ~./ssh ~./gitconfig /etc
+
 # to extract use 7z e  (it will preserve paths)
 
 cp ~/.bash_profile .
